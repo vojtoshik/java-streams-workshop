e6ad3125ccedf575cb36d62734ea0244d7430174
vojtoshik <vojtoshik@gmail.com>
2018-05-05 23:33:07 +0200

    Initial commit

---
+++ src/test/java/com/vojtoshik/education/workshop/streams/BasicTest.java
@@ -0,0 +1,14 @@
+package com.vojtoshik.education.workshop.streams;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicTest {
+
+    @Test
+    public void testThis()
+    {
+        assertTrue(true);
+    }
+}
